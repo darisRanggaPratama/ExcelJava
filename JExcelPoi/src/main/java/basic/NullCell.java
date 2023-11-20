f1035c2edb4b272ca50d13f4271a185828fa9f69
dnl <mayadani28@gmail.com>
2023-11-20 12:14:11 +0700

    Update NullCell.java
    - menggunakan try with resources
    
    - menambahkan method setMissingCellPolicy ke object  workbook.
    
    - lalu diikuti dengan parameter.
    Row.MissingCellPolicy.CREATE_NULL_AS_BLANK

--- JExcelPoi/src/main/java/basic/NullCell.java
+++ JExcelPoi/src/main/java/basic/NullCell.java
@@ -21,6 +21,12 @@ import java.io.IOException;
 import java.util.Optional;
 import java.util.Scanner;
 
+// sample.xlsx
+// GAJI
+// 0
+// 49
+// 0
+// 4
 public class NullCell {
     private static final System.Logger logger = System.getLogger("NullCell");
 
@@ -29,100 +35,83 @@ public class NullCell {
     public static void main(String[] args) {
         try {
             // Get Excel file path
-            System.out.println("\nFile Name:");
+            System.out.println("File Name:");
             String name = scan.nextLine();
             String path = "./src/main/java/trial/";
             String excelFilePath = path + name;
 
             // Open file
             FileInputStream input = new FileInputStream(excelFilePath);
-            Workbook workbook = new XSSFWorkbook(input);
+            FileOutputStream output = null;
+            try (Workbook workbook = new XSSFWorkbook(input)) {
+                workbook.setMissingCellPolicy(Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
 
-            // Get worksheet (GAJI)
-            System.out.println("Sheet Name:");
-            String sheetName = scan.nextLine();
-            Sheet sheet = workbook.getSheet(sheetName);
+                // Get worksheet (GAJI)
+                System.out.println("Sheet Name:");
+                String sheetName = scan.nextLine();
+                Sheet sheet = workbook.getSheet(sheetName);
 
-            // Create new sheet
-            Sheet sheetNew = workbook.createSheet("Null");
+                // Create new sheet
+                Sheet sheetNew = workbook.createSheet("Null");
 
-            // Get Negative. Example: A2 to E46
-            System.out.print("Row:\n  First: ");
-            int firstRow = scan.nextInt();
-            System.out.print("  Last: ");
-            int lastRow = scan.nextInt();
+                // Get Negative. Example: A2 to E46
+                System.out.print("Row:\n  First: ");
+                int firstRow = scan.nextInt();
+                System.out.print("  Last: ");
+                int lastRow = scan.nextInt();
 
-            System.out.print("Column:\n  First: ");
-            int firstCol = scan.nextInt();
-            System.out.print("  Last: ");
-            int lastCol = scan.nextInt();
+                System.out.print("Column:\n  First: ");
+                int firstCol = scan.nextInt();
+                System.out.print("  Last: ");
+                int lastCol = scan.nextInt();
 
-            // Create Cell Title
-            Row rowTitle = sheetNew.createRow(0);
-            Cell titleNo = rowTitle.createCell(0);
-            titleNo.setCellValue("No");
-            Cell cellTitle = rowTitle.createCell(1);
-            cellTitle.setCellValue("Cell Null/Empty");
-            System.out.println("\nNo  Null/Empty");
+                // Create Cell Title
+                Row rowTitle = sheetNew.createRow(0);
+                Cell titleNo = rowTitle.createCell(0);
+                titleNo.setCellValue("No");
+                Cell cellTitle = rowTitle.createCell(1);
+                cellTitle.setCellValue("Cell Null/Empty");
+                System.out.println("\nNo  Null/Empty");
 
-            // Check null/empty/missing/blank cell
-            int rowIndex = 1, rowIdx = 0, colIdx = 0, no = 0;
-            workbook.setMissingCellPolicy(Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
-            for (rowIdx = firstRow; rowIdx <= lastRow; rowIdx++) {
+                // Check null/empty/missing/blank cell
+                int rowIndex = 1, rowIdx = 0, colIdx = 0, no = 0;
+                for (rowIdx = firstRow; rowIdx <= lastRow; rowIdx++) {
 
-                int idxRow = Optional.ofNullable(rowIdx)
-                        .orElse(0);
-
-                Row row = sheet.getRow(idxRow);
-
-                for (colIdx = firstCol; colIdx <= lastCol; colIdx++) {
-
-                    int idxCol = Optional.ofNullable(colIdx)
+                    int idxRow = Optional.ofNullable(rowIdx)
                             .orElse(0);
 
-                    Cell cell = row.getCell(idxCol);
+                    Row row = sheet.getRow(idxRow);
 
-                    // Check null/empty/missing/blank value
-                    if (cell == null || cell.getCellType() == CellType.BLANK ||
-                            (cell.getCellType() == CellType.STRING && cell.getStringCellValue().trim().isBlank()) ||
-                            (cell.getCellType() == CellType.STRING && cell.getStringCellValue().trim().isEmpty())) {
-                        no++;
+                    for (colIdx = firstCol; colIdx <= lastCol; colIdx++) {
 
-                        // Add comment
-                        Comment comment = sheet.createDrawingPatriarch().createCellComment(
-                                new XSSFClientAnchor(0, 0, 0, 0,
-                                        (short) colIdx, rowIdx, (short) (colIdx + 1), rowIdx + 1));
-                        comment.setString(new XSSFRichTextString("Null"));
-                        cell.setCellComment(comment);
+                        int idxCol = Optional.ofNullable(colIdx)
+                                .orElse(0);
 
-                        CellStyle style = workbook.createCellStyle();
-                        style.setFillForegroundColor(IndexedColors.LIGHT_ORANGE.getIndex());
-                        style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-                        cell.setCellStyle(style);
+                        Cell cell = row.getCell(idxCol);
 
-                        // Write blank address cell in new sheet
-                        Row newRow = sheetNew.createRow(rowIndex++);
-                        Cell newNo = newRow.createCell(0);
-                        Cell newCellAddress = newRow.createCell(1);
+                        // Check null/empty/missing/blank value
+                        if (cell == null || (cell.getCellType() == CellType.STRING && cell.getStringCellValue().trim().isEmpty())) {
+                            no++;
 
-                        String cellAddress = CellReference.convertNumToColString(colIdx) + (rowIdx + 1);
-                        newNo.setCellValue(no);
-                        newCellAddress.setCellValue(cellAddress);
+                            // Write blank address cell in new sheet
+                            Row newRow = sheetNew.createRow(rowIndex++);
+                            Cell newNo = newRow.createCell(0);
+                            Cell newCellAddress = newRow.createCell(1);
 
-                        System.out.println(no + "   " + CellReference.convertNumToColString(colIdx) + (rowIdx + 1));
+                            String cellAddress = CellReference.convertNumToColString(colIdx) + (rowIdx + 1);
+                            newNo.setCellValue(no);
+                            newCellAddress.setCellValue(cellAddress);
+
+                            System.out.println(no + "   " + CellReference.convertNumToColString(colIdx) + (rowIdx + 1));
+                        }
                     }
                 }
+
+                // Save file
+                output = new FileOutputStream("./src/main/java/output/Nulls.xlsx");
+                workbook.write(output);
+
             }
-
-            // Save file
-            FileOutputStream output = new FileOutputStream("./src/main/java/output/Nulls.xlsx");
-            workbook.write(output);
-
-            // Close Excel file
-            workbook.close();
-            scan.close();
-            input.close();
-            output.close();
         } catch (IOException e) {
             String noFile = "File not found: \n" + e;
             logger.log(System.Logger.Level.ERROR, noFile);

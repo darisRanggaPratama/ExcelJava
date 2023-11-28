a9dfba38f47cef4802479a1979ebb5a21005b7da
DESKTOP-7H428EE\User <daris.rangga.pratama@gmail.com>
2023-11-28 11:12:16 +0700

    initial commit

--- JExcelPoi/src/main/java/basic/Main.java
+++ JExcelPoi/src/main/java/basic/Main.java
@@ -2,6 +2,7 @@ package basic;
 
 public class Main {
     public static void main(String[] args) {
+        Data.inputData();
         DecimalCell.checkDecimal();
         ErrCell.checkErrCell();
         ErrCells.checkErrCells();
